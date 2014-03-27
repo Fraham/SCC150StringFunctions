6dded7952a2ad0ed83ca3d3903899390d5febe6e
Graham Pye <frahamp@gmail.com>
2014-03-27 21:28:37 +0000

    Adding comments

--- String Functions/mystring.c
+++ String Functions/mystring.c
@@ -41,36 +41,36 @@ int mystrcmp(const char *s1, const char *s2)
 
 char *mystrncpy(char *dst, const char *src, int n)
 {
-	int count  = 0;
+	int count  = 0; // used to keep track of the index and to make sure it doesnt go above the limit
 	
-	while (*s1 != '\0' && count < n)
+	while (*s1 != '\0' && count < n) // will stop when s1 reaches null and not gone past the limit
 	{
-		*dst = *src;
+		*dst = *src; // getting the character from the source string.
 		
-		dst++;
+		dst++; // incrementing the to the next character.
 		src++;
-		count++;
+		count++; // incrementing the index.
 	}
 	
-	*dst = '\0';
+	*dst = '\0'; // ending the null character on the end.
 	
 	return dst;
 }
 
 char *mystrcpy(char *dst, const char *src)
 {
-	int count  = 0;
+	int count  = 0; // used to keep track of the index
 	
-	while (*s1 != '\0')
+	while (*s1 != '\0') // will stop when s1 reaches null
 	{
-		*dst = *src;
+		*dst = *src;  // getting the character from the source string.
 		
-		dst++;
+		dst++;  // incrementing the to the next character.
 		src++;
-		count++;
+		count++;  // incrementing the index.
 	}
 	
-	*dst = '\0';
+	*dst = '\0'; // ending the null character on the end.
 	
 	return dst;
 }
@@ -80,9 +80,9 @@ char *mystrdup(const char *src)
 {
     char *p = malloc(strlen(s) + 1);// need the added one to be have the null character at the end
 	
-	if(p) 
+	if(p)  // if p is valid.
 	{ 
-		strcpy(p, s); 
+		strcpy(p, s); // calling the copy function.
 	}
 	
 	return p;
